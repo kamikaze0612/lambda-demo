88f3ca7c7410e353dfef3c2d360de8c109fd8e38
kamikaze0612 <buyan.bekh@gmail.com>
2025-05-12 21:40:10 +0800

    init

---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  treeshake: true,
+  dts: true,
+  splitting: true,
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  sourcemap: true,
+});
